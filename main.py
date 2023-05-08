2a0e7747bd6486a5506778044df9556dd51f198a
CJ <christopher.galliart@gmail.com>
2023-05-08 13:19:17 -0500

    Refactor

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import numpy as np
 import pandas as pd
-from sklearn import preprocessing
-from sklearn.linear_model import LogisticRegressionCV
+from sklearn import preprocessing, linear_model
 from flask import Flask, request
 
 app = Flask(__name__)
@@ -11,25 +10,14 @@ def logistic_regression():
     if request.method == 'GET':
       return "Hello"
     if request.method == 'POST':
-      params = request.get_json(force=True)
-      print("Params:  {}".format(params))
-      body = dict(params)
+      body = request.get_json(force=True)
       return create_logistic_regression(body)	
 
 def create_logistic_regression(body):
-  sentiment = pd.DataFrame(body['sentiment'])
-  sentimentDf = pd.get_dummies(sentiment.astype('category'), prefix='Test')
-  sentimentDf = check_sentiment_data(sentimentDf)
-  starter = [body['close'], body['volume'], body['positive'], body['negative']]
-  for i in sentimentDf:
-    holder = list(sentimentDf[i])
-    starter.append(holder)  
-  df = np.array(starter)
-  score = {}
-  score["next"] = logistic_regression(1,df)
-  score["week"] = logistic_regression(10,df)
-  score["month"] = logistic_regression(20,df)
-  score["ticker"] = body['ticker']
+  sentiment = pd.get_dummies(pd.DataFrame(body['sentiment']).astype('category'), prefix='Test')
+  sentiment = check_sentiment_data(sentiment)
+  df = np.array([body['close'], body['volume'], body['positive'], body['negative']] + sentiment.T.values.tolist())
+  score = {"next": logistic_regression(1,df), "week": logistic_regression(10,df), "month": logistic_regression(20,df), "ticker": body['ticker']}
   return score
 
 def check_sentiment_data(df):
@@ -41,18 +29,15 @@ def check_sentiment_data(df):
 
 def logistic_regression(time, df):
   key = create_key_for_logistic_regression(time, df)
-  reshapedDf = df.reshape(len(df[0]),8)
-  processedDf = preprocessing.StandardScaler().fit(reshapedDf[:len(reshapedDf)-time,:])
-  normalizedDf = processedDf.transform(reshapedDf[:len(reshapedDf)-time,:])
-  logReg = LogisticRegressionCV(cv=8, random_state=0).fit(normalizedDf,key)
-  prediction = logReg.predict(reshapedDf[:1,:])
-  score = str(logReg.score(normalizedDf,key))
+  processedDf = preprocessing.StandardScaler().fit_transform(df[:len(df)-time,:8])
+  logReg = linear_model.LogisticRegressionCV(cv=8, random_state=0).fit(processedDf,key)
+  prediction = logReg.predict(df[:1,:8])
+  score = str(logReg.score(processedDf,key))
   return score + " " + str(prediction[0])
 
 def create_key_for_logistic_regression(n, df):
-  closeOfDay = [0] * (len(df[0]) - n)
-  for i in range(0,len(df[0])):
-      a = df[0][i]
+  closeOfDay = np.zeros(len(df[0])-n)
+  for i, a in enumerate(df[0]):
       if i < len(df[0]) - n:
         holder = df[0][i + n]
         closeOfDay[i] = 1 if (a - holder) > 0 else 0
@@ -60,4 +45,3 @@ def create_key_for_logistic_regression(n, df):
 
 if __name__ == "__main__":
   app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
-  
